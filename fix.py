0d69d88b6081197076a942bde8821bfbf8985d5e
Johannes Neumeier <hello@johannesneumeier.com>
2021-12-15 16:03:24 +0200

    WIP, subdir and permalinks conflicting, e.g. /blog

--- fix.py
+++ fix.py
@@ -31,13 +31,30 @@ for dir in os.listdir(microsites_repo):
 
 
 # Remove cachebusting timestamps from files
+images = {}
 for path in glob.glob(f"{DIR}/**/*.jpg?*", recursive=True):
     file = re.sub(r"\?[0-9]*$", "", path)
     print(f"Save file without cachebuster: {file}")
     os.rename(path, file)
+    images[path] = file
+
+# Fix image links in files
+for path in glob.glob(f"{DIR}/**/*", recursive=True):
+    html = ""
+    try:
+        with open(path, "r") as f:
+            html = f.read()
+            html = re.sub("?", "?", html)
+        for cached, without in images.items():
+            html = re.sub(cached, without)
+        with open(path, "w") as f:
+            f.write(html)
+    except Exception as e:
+        print(path, e)
 
 # Fix things in scraped html
 done = []
+permalinks = {}
 for path in glob.glob(f"{DIR}/**/*.html", recursive=True):
 
     print(f"Fix things in {path}")
@@ -60,14 +77,16 @@ for path in glob.glob(f"{DIR}/**/*.html", recursive=True):
         html = re.sub(r"tester\">Fonts</a>",
                       "fonts\">Fonts</a>",
                       html)
-        html = re.sub(r"</head>",
-                      "<meta rel=\"canonical\" href=\"/%s\">\n</head>"
-                      % os.path.splitext(os.path.basename(path))[0],
-                      html)
         html = re.sub(r"href=\"(.*)\" rel=\"home\">",
                       "href=\"/\" rel=\"home\">",
                       html)
         html = re.sub(r"\.jpg\?[0-9]*", ".jpg", html)
+
+        if "meta rel\"canonical" not in html:
+            html = re.sub(r"</head>",
+                        "<meta rel=\"canonical\" href=\"/%s\">\n</head>"
+                        % os.path.splitext(os.path.basename(path))[0],
+                        html)
 
         doctype = """<!DOCTYPE html>
 <!--[if IE 8]>    <html class="no-js no-svg lt-ie9 responsive"> <![endif]-->
@@ -78,13 +97,24 @@ for path in glob.glob(f"{DIR}/**/*.html", recursive=True):
 
         if html.startswith("---"):
             html = html.split("\n")
-            html = "\n".join(html[3:])
+            html = "\n".join([r for r in html[3:] if r.strip() != ""])
 
         if not html.startswith("---"):
             perma = os.path.splitext(path)[0][1:]
+            permalinks[os.path.basename(path)] = perma
             html = f"---\npermalink: {perma}\n---\n" + html
 
     with open(path, "w") as f:
         f.write(html)
 
     done.append(path)
+
+# Fixing permalinks to html files
+for path in glob.glob(f"{DIR}/**/*.html", recursive=True):
+    html = ""
+    with open(path, "r") as f:
+        html = f.read()
+    for file, permalink in permalinks.items():
+        html = re.sub(file, os.path.splitext(file)[0], html)
+    with open(path, "w") as f:
+        f.write(html)
