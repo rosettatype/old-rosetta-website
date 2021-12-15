bf974af4597c42b0154da588efb9265ac1259dde
Johannes Neumeier <hello@johannesneumeier.com>
2021-12-15 19:18:40 +0200

    Sort of fix the FIU modal next/prev

--- fix.py
+++ fix.py
@@ -93,6 +93,9 @@ for path in glob.glob(f"{DIR}/**/*.html", recursive=True):
                       "href=\"/\" rel=\"home\">",
                       html)
         html = re.sub(r"\.jpg\?[0-9]*", ".jpg", html)
+        html = re.sub('<a href="fonts-in-use#',
+                      '<a href="/fonts-in-use#',
+                      html)
 
         if "meta rel\"canonical" not in html:
             html = re.sub(r"</head>",
